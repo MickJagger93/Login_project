a667a6aab78774864d8d87c9f1b69e36b4374dea
JaggerMQ <quevedo993mi@gmail.com>
2026-04-04 14:19:33 +0530

    gitignore and app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-"""
-app.py
-
-Archivo principal de la aplicación Flask para el sistema de autenticación de usuarios.
-Configura la aplicación, la base de datos, la gestión de sesiones y registra los blueprints.
-"""
-
 from flask import Flask
 from models import db
 from config import Config
@@ -12,18 +5,6 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 
 def create_app():
-    
-    """
-    Crea y configura la instancia principal de la aplicación Flask.
-
-    - Inicializa la configuración desde el objeto Config.
-    - Inicializa la base de datos y las migraciones.
-    - Configura Flask-Login para la gestión de sesiones de usuario.
-    - Registra los blueprints de autenticación y rutas principales.
-
-    Returns:
-        app (Flask): La aplicación Flask configurada.
-    """
     
     app = Flask(__name__)
     app.config.from_object(Config)
@@ -35,16 +16,6 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(user_id):
-
-        """
-        Carga un usuario desde la base de datos por su ID.
-
-        Args:
-            user_id (int): ID del usuario.
-
-        Returns:
-            User: Instancia del usuario si existe, si no, None.
-        """
 
         return db.session.get(User, user_id)
 
